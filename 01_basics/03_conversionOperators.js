2842c9165a869ac1fa71d356f815d12eed4e4cc0
Nisarg Panchal <153318941+Nishh2303@users.noreply.github.com>
2025-08-27 11:28:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score)); 
+
+let valuesInNumber = Number(score)
+// console.log(typeof valuesInNumber);
+// console.log(valuesInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "nishh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

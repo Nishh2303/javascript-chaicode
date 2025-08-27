244636395f8941cad724fcebce54bc69058ee3fe
Nisarg Panchal <153318941+Nishh2303@users.noreply.github.com>
2025-08-27 11:12:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treats all JS code as newer version 
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3); // code readability should be high
+
+
+console.log("NishhOP");
+
+let name = "nisarg"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned value 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // objects
